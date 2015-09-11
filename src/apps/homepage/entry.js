6f124dac55772742034426d41d6c74dd9b60af51
Rich Soni <rich@richsoni.com>
2015-09-11 12:24:24 -0400

    top nav

--- src/apps/homepage/entry.js
+++ src/apps/homepage/entry.js
@@ -1,10 +1,38 @@
 React = require("react")
+socialButtonStyle = require("./socialButton.css")
+
+class SocialButton extends React.Component{
+  render(){
+    return <li>
+      <a
+        title={this.props.service}
+        href={this.props.href}
+      >
+        <i className={`fa fa-${this.props.service}`}></i>
+        <span>{this.props.children}</span>
+      </a>
+    </li>
+  }
+}
+
+SocialButton.propTypes = {
+  service: React.PropTypes.string,
+  href: React.PropTypes.string,
+}
 
 class RootComponent extends React.Component{
   render() {
     return <header>
-      <nav>
-        <li>Yo</li>
+      <nav className='top-nav' id='social-nav'>
+        <SocialButton service='soundcloud' href='https://soundcloud.com/richsoni' />
+        <SocialButton service='twitter' href='https://twitter.com/richsoni'/>
+        <SocialButton service='github' href='https://github.com/richsoni'/>
+        <SocialButton service='linkedin' href='https://www.linkedin.com/in/richsoni'/>
+      </nav>
+      <nav className='top-nav' id='sign-up-nav'>
+        <SocialButton href='http://richsoni.com/subscribe' service='envelope'>
+          Sign Up
+        </SocialButton>
       </nav>
     </header>
   }
