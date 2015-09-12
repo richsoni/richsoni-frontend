f1f476f43727a103c66778a7fcf8d981aed84318
Rich Soni <rich@richsoni.com>
2015-09-11 23:42:50 -0400

    i think homepage (small is done for mvp)

--- src/apps/homepage/entry.js
+++ src/apps/homepage/entry.js
@@ -15,36 +15,37 @@ class SocialButton extends React.Component{
   }
 }
 
-SocialButton.propTypes = {
-  service: React.PropTypes.string,
+SocialButton.propTypes = { service: React.PropTypes.string,
   href: React.PropTypes.string,
 }
 
 class RootComponent extends React.Component{
   render() {
-    return <header id='homepage-header'>
-      <nav className='top-nav' id='social-nav'>
-        <SocialButton service='soundcloud' href='https://soundcloud.com/richsoni' />
-        <SocialButton service='twitter' href='https://twitter.com/richsoni'/>
-        <SocialButton service='github' href='https://github.com/richsoni'/>
-        <SocialButton service='linkedin' href='https://www.linkedin.com/in/richsoni'/>
-      </nav>
-      <nav className='top-nav' id='logo-nav'>
-        <img src='/images/avatar.png' />
-      </nav>
-      <nav className='top-nav' id='internal-nav'>
-        <li><a>Blog</a></li>
-        <li><a>Songs</a></li>
-      </nav>
-      <nav className='top-nav' id='sign-up-nav'>
-        <SocialButton href='http://richsoni.com/subscribe' service='envelope'>
-          Sign Up
-        </SocialButton>
-        <span>
-          Newsletter subscribers get a FREE and exclusive material... Including an UNRELEASED song immediately!!!
-        </span>
-      </nav>
-    </header>
+    return <div>
+      <header id='homepage-header'>
+        <nav className='top-nav' id='social-nav'>
+          <SocialButton service='soundcloud' href='https://soundcloud.com/richsoni' />
+          <SocialButton service='twitter' href='https://twitter.com/richsoni'/>
+          <SocialButton service='github' href='https://github.com/richsoni'/>
+          <SocialButton service='linkedin' href='https://www.linkedin.com/in/richsoni'/>
+        </nav>
+        <nav className='top-nav' id='logo-nav'>
+          <img src='/images/avatar.png' />
+        </nav>
+        <nav className='top-nav' id='internal-nav'>
+          <li><a>Blog</a></li>
+          <li><a>Songs</a></li>
+        </nav>
+        <nav className='top-nav' id='sign-up-nav'>
+          <SocialButton href='http://richsoni.com/subscribe' service='envelope'>
+            Sign Up
+          </SocialButton>
+          <span>
+            Newsletter subscribers get a FREE and exclusive material... Including an UNRELEASED song immediately!!!
+          </span>
+        </nav>
+      </header>
+    </div>
   }
 }
 
