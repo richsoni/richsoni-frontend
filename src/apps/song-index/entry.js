6d769ec208943584a2462402b10af6154ab2ec03
Rich Soni <rich@richsoni.com>
2015-09-16 16:00:13 -0400

    add featured songs

--- src/apps/song-index/entry.js
+++ src/apps/song-index/entry.js
@@ -7,6 +7,7 @@ const Footer          = require("../../shared/footer/component")
 const parseCollection = require("../../lib/parseCollection")
 const ajax            = require("../../lib/ajax")
 
+const SOUNDCLOUD_BASE = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/95252923&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"
 class Song extends React.Component {
   render(){
     return <li>
@@ -27,9 +28,16 @@ class RootComponent extends React.Component{
   }
 
   render() {
-    return <div style={{marginTop: '3em'}}>
+    return <div className='song-index-wrap'>
       <Header />
-      {this._renderSongs()}
+      <div className='playlist-container'>
+        <h2>Featured</h2>
+        <iframe height='100%' scrolling="no" frameborder="no" src={SOUNDCLOUD_BASE}></iframe>
+      </div>
+      <div className='songs-container'>
+        <h2>All Songs</h2>
+        {this._renderSongs()}
+      </div>
       <Footer />
     </div>
   }
