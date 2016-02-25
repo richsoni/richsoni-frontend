97e190f1a3454370a9f5a8f75748e7b51f84ccac
Rich Soni <rich@richsoni.com>
2016-02-25 16:52:08 -0500

    ...

--- src/apps/blog-index/post-grid.js
+++ src/apps/blog-index/post-grid.js
@@ -27,7 +27,7 @@ class PostGrid extends React.Component {
           backgroundSize: 'cover',
           backgroundRepeat: 'no-repeat',
           backgroundPosition: 'center',
-          backgroundColor: '#fffffff',
+          backgroundColor: '#bebebe',
         }} />
       <h1 style={{
         marginBottom: '.4em',
